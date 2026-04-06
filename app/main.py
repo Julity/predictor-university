b61ead60f1b58643dd285c697c6d6bd09db3602e
Julity <131943231+Julity@users.noreply.github.com>
2026-04-06 09:51:10 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -127,7 +127,7 @@ logging.basicConfig(level=logging.INFO)
 st.set_page_config(page_title="🎓 RANK FORECAST", layout="wide")
 
 # 5. ПРОСТОЙ ИНТЕРФЕЙС ДО ЗАГРУЗКИ МОДЕЛЕЙ
-st.title("🎓 RANK FORECAST - Универсальная модель")
+st.title("🎓 Прогностическая модель управления академической конкурентоспособностью")
 
 
 
